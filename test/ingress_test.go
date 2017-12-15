55b5890577ece1f5a04afff1d3b744c9f9adf6f6
yue9944882 <291271447@qq.com>
2017-12-15 18:46:55 +0800

    feat(test): ingress ci test cases

---
+++ test/ingress_test.go
@@ -0,0 +1,60 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	kubernetes "github.com/yue9944882/kubernetes-client-lambda"
+	api_ext_v1 "k8s.io/api/extensions/v1beta1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func TestIngressperation(t *testing.T) {
+	ns := "default"
+
+	success, err := kubernetes.Ingress.OutOfCluster(restconfig).InNamespace(ns).Add(
+		func() *api_ext_v1.Ingress {
+			var ing api_ext_v1.Ingress
+			ing.Name = "foo"
+			ing.Namespace = ns
+			ing.Spec.Rules = []api_ext_v1.IngressRule{
+				api_ext_v1.IngressRule{
+					Host: "xxx.com",
+					IngressRuleValue: api_ext_v1.IngressRuleValue{
+						HTTP: &api_ext_v1.HTTPIngressRuleValue{
+							Paths: []api_ext_v1.HTTPIngressPath{
+								api_ext_v1.HTTPIngressPath{
+									Path: "/",
+									Backend: api_ext_v1.IngressBackend{
+										ServiceName: "foo",
+										ServicePort: intstr.FromInt(80),
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			return &ing
+		},
+	).CreateIfNotExist()
+	assert.Equal(t, true, success, "failed to create pod")
+	assert.NoError(t, err, "some error happened")
+	success, err = kubernetes.Ingress.OutOfCluster(restconfig).InNamespace(ns).Grep(func(ing *api_ext_v1.Ingress) bool {
+		return ing.Name == "foo"
+	}).Map(func(ing *api_ext_v1.Ingress) *api_ext_v1.Ingress {
+		if ing.Labels == nil {
+			ing.Labels = make(map[string]string)
+		}
+		ing.Labels["test"] = "yes"
+		return ing
+	}).Update()
+	assert.Equal(t, true, success, "failed to update pod")
+	assert.NoError(t, err, "some error happened")
+	success, err = kubernetes.Ingress.OutOfCluster(restconfig).InNamespace(ns).Grep(func(ing *api_ext_v1.Ingress) bool {
+		return ing.Name == "foo"
+	}).Delete()
+	assert.Equal(t, true, success, "failed to delete pod")
+	assert.NoError(t, err, "some error happened")
+}
